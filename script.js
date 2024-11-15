928b40f99409eaea346d2d6f33f88cd68924fe5c
Your Name <your_email@example.com>
2024-11-15 20:24:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchBtn = document.getElementById('btn');
+const word = document.querySelector('input');
+const result = document.getElementById('resultContainer');
+
+searchBtn.addEventListener('click', async () => {
+    const words = word.value;
+    result.innerHTML = '';
+    if (!words){
+        result.innerHTML = '<p>Please enter a word to search.</p>';
+        return;
+    }
+    const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${words}`;
+    fetch(URL)
+       .then((response) =>{
+           if(!response.ok){
+              throw new Error('Word not found');
+           }
+           return response.json();
+       })
+       .then((data) => {
+           const meaning = data[0].meanings[0];
+           const definition = meaning.definitions[0].definition;
+           result.innerHTML = `<h3>word: ${words}</h3>
+           <p><strong>Defination:</strong> ${definition}</p>
+           <p><strong>Part of speech: </strong> ${meaning.partOfSpeech}</p>
+           `;
+       })
+       .catch((error) =>{        
+        result.innerHTML = `<p>${error.message}</p>`;
+       });
+});
+
+
